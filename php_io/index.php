e245488b71a076d54e49f0282df95a2be16c9541
Ken Usa <kusa_230000002006@uic.edu.ph>
2025-01-09 14:08:09 +0800

    Add files via upload

--- php_io/index.php
+++ php_io/index.php
@@ -1,43 +1,22 @@
-<?php
-
-
-
-
-
-
-
-
-if(isset($_GET('btn_submit'))){
-    $name = $_GET ['fname'];
-    $address = $_GET ['address'];
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Enrollment Form</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action=""method="get">
-        <h1>Enrollment Form </h1>
+    <form action="index2.php" method="get"> 
+        <h1>Enrollment Form</h1>
         <hr>
-        <label for ="fname">Student Name:</label>
-        <input type="text" name="fname" id="fname">
+        <label for="fname">Student Name:</label>
+        <input type="text" name="fname" id="fname" required>
         <br>
-        <label for ="lname">Living Address:</label>
-        <input type="text" name="address" id="address">
+        <label for="address">Living Address:</label>
+        <input type="text" name="address" id="address" required>
         <br>
         <button type="submit" name="btn_submit">Submit</button>
     </form>
-    
-    
-    
 </body>
 </html>
